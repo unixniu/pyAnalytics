a6f961cff54c7b179511c6173cba1ed607c4abf4
unixniu <william.niu@live.com>
2016-06-28 22:52:11 +0800

    implement takeoff analysis

--- collector.py
+++ collector.py
@@ -7,11 +7,12 @@ import os
 import random
 import os.path
 
-HIST_DATA_PATH = r'e:\analytics\stock\hist-0610'
+HIST_DATA_PATH = r'e:\analytics\stock\hist-W-2016'
+REPORT_DATA_PATH = r'e:\analytics\stock\report'
 CURRENT_TERM = 2016, 1
 
 
-def get_hist_data(start=None, end=None, exclude_cyb=True, sample=0, persist=False):
+def get_hist_data(start=None, end=None, type='D', exclude_cyb=True, sample=0, persist=False):
     basics = ts.get_stock_basics()
     codes = basics.index if not exclude_cyb else [x for x in basics.index if not x.startswith('300')]
     hist_data = {}
@@ -20,14 +21,29 @@ def get_hist_data(start=None, end=None, exclude_cyb=True, sample=0, persist=Fals
         os.makedirs(HIST_DATA_PATH)
     for code in codes_selected:
         try:
-            df = ts.get_hist_data(code, start, end)
+            df = ts.get_hist_data(code, start, end, ktype=type)
             hist_data[code] = df
             if persist:
                 df.to_csv(os.path.join(HIST_DATA_PATH, '%s.csv' % code))
             print('retrieved hist data for %s' % code)
         except Exception as ex:
-            print('error occurred in retrieving %s:%s' % code, ex)
+            try:
+                print('error occurred in retrieving {}: {}'.format(code, ex))
+            except Exception as innerex:
+                print('exception: {}'.format(innerex))
     return pd.Panel(hist_data)
+
+
+def get_report_data(year, term):
+    try:
+        if not os.path.isdir(REPORT_DATA_PATH):
+            os.makedirs(REPORT_DATA_PATH)
+        df = ts.get_report_data(year, term)
+        path = os.path.join(REPORT_DATA_PATH, '{}-{}.csv'.format(year, term))
+        df.to_csv(path)
+    except Exception as ex:
+        print("error occurred in retrieving report data: ", ex)
+
 
 def find_lis(s):
     # files = sc.wholeTextFiles(DATA_FILE_PATH, use_unicode=False)
@@ -55,10 +71,9 @@ def get_financial_data(terms=1, exclude_cyb=True, sample=0, persist=False):
 
 
 if __name__ == "__main__":
-    # get_hist_data(start='2016-01-01', exclude_cyb=True, persist=True, sample=20)
-    s = pd.Series(np.random.randint(1, 100, 80))
-    print(list(s))
-    lis = find_lis(s)
-    print(lis)
-
+    # get_hist_data(start='2016-01-01', exclude_cyb=True, type='W', persist=True)
+    get_report_data(2016, 1)
+    get_report_data(2015, 4)
+    get_report_data(2015, 3)
+    get_report_data(2015, 2)
 
